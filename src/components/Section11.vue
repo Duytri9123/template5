580ef115ba2102d54f7470a9058f9e4863ee9b6b
duytris <tailieuonthi2003@gmail.com>
2025-08-06 11:25:31 +0700

    sua loi section11

--- src/components/Section11.vue
+++ src/components/Section11.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="relative  md:mt-10 lg:mt-[10%] xl:mt-[4%] 2xl:mt-[6%]">
-    <div class="relative w-full flex flex-col justify-center items-center gap-10 bg-background-rev overflow-hidden z-10">
+    <div class="relative w-full flex flex-col justify-center items-center gap-10 bg-background-rev overflow-hidden z-10 2xl:py-[5%] lg:py-[5%] py-[3%]">
       <div class="absolute w-full opacity-25">
         <img src="../Images/hinhanh.png" alt="" class="object-cover lg:w-full h-[100dvh]" />
       </div>
       <h1
-        class="relative xl:w-1/2 lg:w-3/5 text-lg md:text-xl lg:text-3xl xl:text-[38px] font-bold text-center px-4"
+        class="text-white relative xl:w-1/2 lg:w-3/5 text-lg md:text-xl lg:text-3xl xl:text-[38px] font-bold text-center px-4"
       >
         Cảm nhận của khách hàng sau khi sử dụng dịch vụ Tầm soát ung thư tuyến giáp
       </h1>
